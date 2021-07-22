4498c5430aa6ca8913a5b0d9a3bbb3d244458dc5
Emanuele Ballarin <emanuele@ballarin.cc>
2021-07-22 11:15:04 +0200

    Add files via upload

---
+++ src/train.py
@@ -0,0 +1,113 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ============================================================================ #
+# Part of:
+# "Whole-system multidimensional financial time series prediction and simulation
+# from timestamped prices only"
+#
+# Davide Roznowicz, Emanuele Ballarin <emanuele@ballarin.cc>
+#
+# (https://github.com/emaballarin/financial-wholenamycs)
+# ============================================================================ #
+
+# Imports
+import torch as th
+from torch.nn import MSELoss
+
+from data import stock_dataloader_dispatcher
+from architectures import StockTransformerModel
+
+from ebtorch.optim import Lookahead, RAdam
+from ebtorch.logging import AverageMeter
+
+from train_utils import train_epoch
+
+# DATA:
+train_loader, test_loader, _, data_props = stock_dataloader_dispatcher(
+    data_path="../data/",
+    which_financial=(0, 1, 2),
+    which_contextual=(0, 1, 2, 3, 4),
+    time_lookback=20,
+    time_predict=5,
+    window_stride=1,
+    ttsr=0.8,
+    train_bs=2,
+    test_bs=512,
+)
+
+# MODEL PARAMETERS:
+A = [
+    (
+        data_props.fin_size,
+        [1, 2],
+        [2, 4],
+        [2, 2],
+        [3, 1],
+    ),
+    {"batchnorm": [True, True]},
+]
+B = [([12, 2, 2], 3)]
+C = [(20, 2, 10), {"batch_first": True}]
+D = [
+    (),
+    {"encoder_layer": "_", "num_layers": 2},
+]
+E = [([20 * 6, 10], 15)]
+
+F = data_props.ctx_size
+G = data_props.fin_size
+
+# MODEL:
+
+
+################################################################################
+
+nrepochs = 2
+AUTODETECT: bool = True
+device = th.device("cuda" if th.cuda.is_available() and AUTODETECT else "cpu")
+model = StockTransformerModel(A, B, C, D, E, F, G).to(device)
+criterion = MSELoss(reduce=True)
+optimizer = RAdam(model.parameters())
+train_acc_avgmeter = AverageMeter("Training Loss")
+# base_optimizer = MadGrad(model.parameters(), lr=0.00017)
+# optimizer = Lookahead(base_optimizer, la_steps=4)
+# scheduler = MultiStepLR(optimizer, milestones=[10, 11], gamma=0.4)
+
+################################################################################
+
+for epoch in range(1, nrepochs + 1):
+
+    # Training
+    print("TRAINING...")
+
+    train_epoch(
+        model=model,
+        device=device,
+        train_loader=train_loader,
+        loss_fn=criterion,
+        optimizer=optimizer,
+        epoch=epoch,
+        print_every_nep=1,
+        train_acc_avgmeter=train_acc_avgmeter,
+        inner_scheduler=None,
+        quiet=False,
+    )
+
+    # Tweaks for the Lookahead optimizer (before testing)
+    if isinstance(optimizer, Lookahead):
+        optimizer._backup_and_load_cache()
+
+    # Testing: on training and testing set
+    #    print("\nON TRAINING SET:")
+    #    _ = test(model, device, test_on_train_loader, lossfn, quiet=False)
+    #    print("\nON TEST SET:")
+    #    _ = test(model, device, test_loader, lossfn, quiet=False)
+    #    print("\n\n")
+
+    # Tweaks for the Lookahead optimizer (after testing)
+    if isinstance(optimizer, Lookahead):
+        optimizer._clear_and_load_backup()
+
+    # Scheduling step (outer)
+    # scheduler.step()
---
+++ src/train_utils.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+# ============================================================================ #
+# Part of:
+# "Whole-system multidimensional financial time series prediction and simulation
+# from timestamped prices only"
+#
+# Davide Roznowicz, Emanuele Ballarin <emanuele@ballarin.cc>
+#
+# (https://github.com/emaballarin/financial-wholenamycs)
+# ============================================================================ #
+
+# Imports
+import torch as th
+
+def train_epoch(
+    model,
+    device,
+    train_loader,
+    loss_fn,
+    optimizer,
+    epoch,
+    print_every_nep,
+    train_acc_avgmeter,
+    inner_scheduler=None,
+    quiet=False,
+):
+    train_acc_avgmeter.reset()
+    model.train()
+    for batch_idx, batched_datapoint in enumerate(train_loader):
+
+        # NOTE: Data-specific!
+        data, target_ = batched_datapoint
+        target = th.flatten(target_, start_dim=2, end_dim=3).transpose(-1, -2)
+
+        data, target = data.to(device), target.to(device)
+        optimizer.zero_grad()
+        output = model(data)
+        loss = loss_fn(output, target)
+        loss.backward()
+
+        optimizer.step()
+        if inner_scheduler is not None:
+            inner_scheduler.step()
+
+        train_acc_avgmeter.update(loss.item())
+
+        if not quiet and batch_idx % print_every_nep == 0:
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tAvg. loss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    train_acc_avgmeter.avg,
+                )
+            )
+
+
+def test(model, device, test_loader, loss_fn, test_acc_avgmeter, quiet=False):
+
+    test_acc_avgmeter.reset()
+    model.eval()
+    with th.no_grad():
+
+        # NOTE: Data-specific!
+        for batched_datapoint in test_loader:
+            data, target_ = batched_datapoint
+            target = th.flatten(target_, start_dim=2, end_dim=3).transpose(-1, -2)
+
+            data, target = data.to(device), target.to(device)
+            output = model(data)
+
+            loss = loss_fn(output, target)
+            test_acc_avgmeter.update(loss.item())
+
+    if not quiet:
+        print("Average loss: {:.4f})".format(test_acc_avgmeter.avg))
+    return test_acc_avgmeter.avg
